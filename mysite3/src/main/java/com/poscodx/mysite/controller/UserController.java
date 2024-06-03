a1afbb3c105c520490feb5d8bf9f4536d162d344
yusubhin <sdbtnqlss@gmail.com>
2024-06-03 14:53:59 +0900

    practice #mysite3 - controller, user 에노테이션 추가

--- mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.poscodx.mysite.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.poscodx.mysite.service.UserService;
 
+@Controller
 public class UserController {
 	@Autowired
 	private UserService userService;
--- mysite3/src/main/java/com/poscodx/mysite/repository/UserRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/UserRepository.java
@@ -6,8 +6,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.springframework.stereotype.Repository;
+
 import com.poscodx.mysite.vo.UserVo;
 
+@Repository
 public class UserRepository {
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
