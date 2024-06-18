3793e3f9c589ecfb5408e1a3177b5ac41f5c928b
yusubhin <sdbtnqlss@gmail.com>
2024-06-18 14:54:39 +0900

    Java Config #3

---
+++ mysite4/src/main/java/com/poscodx/mysite/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.poscodx.mysite.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@EnableAspectJAutoProxy
+@Import({})
+public class WebConfig {
+
+}
--- mysite4/src/main/java/com/poscodx/mysite/config/app/DBConfig.java
+++ mysite4/src/main/java/com/poscodx/mysite/config/app/DBConfig.java
@@ -12,7 +12,7 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.TransactionManager;
 
 @Configuration
-@PropertySource("classpath:com/poscodx/mysite/app/jdbc.properties")
+@PropertySource("classpath:com/poscodx/mysite/config/app/jdbc.properties")
 public class DBConfig {
 	@Autowired
 	private Environment env;
@@ -22,8 +22,6 @@ public class DBConfig {
 		BasicDataSource dataSource = new BasicDataSource();
 		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
 		dataSource.setUrl(env.getProperty("jdbc.url"));
-		//dataSource.setUrl("jdbc:mariadb://192.168.35.55:3306/webdb?charset=utf8");
-		//dataSource.setUrl("jdbc:mariadb://172.20.10.11:3306/webdb?charset=utf8");
 		dataSource.setUsername(env.getProperty("jdbc.username"));
 		dataSource.setPassword(env.getProperty("jdbc.password"));
 		dataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class));
