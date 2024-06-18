b4598305d0e9c49f4c407a56d4ae2bf53a77b8a1
yusubhin <sdbtnqlss@gmail.com>
2024-06-18 13:49:44 +0900

    java config #2

--- mysite4/src/main/java/com/poscodx/mysite/config/app/DBConfig.java
+++ mysite4/src/main/java/com/poscodx/mysite/config/app/DBConfig.java
@@ -1,8 +1,39 @@
 package com.poscodx.mysite.config.app;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 @Configuration
+@PropertySource("classpath:com/poscodx/mysite/app/jdbc.properties")
 public class DBConfig {
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public DataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		//dataSource.setUrl("jdbc:mariadb://192.168.35.55:3306/webdb?charset=utf8");
+		//dataSource.setUrl("jdbc:mariadb://172.20.10.11:3306/webdb?charset=utf8");
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		dataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class));
+		dataSource.setMaxActive(env.getProperty("jdbc.maxActive", Integer.class));
+		
+		return dataSource;
+	}
 
+	@Bean
+	public TransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
 }
